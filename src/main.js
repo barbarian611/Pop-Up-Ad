b688969497afa99f86430353e7955fef56afceab
Max <max.lee.092691@gmail.com>
2020-01-30 13:53:34 -0500

    Pop-up-Ad

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './app.scss';
+import Popup from './components/Popup'
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+const headerElement = <h1>shmuganamugana</h1>
+
+ReactDOM.render(
+  <Popup />,
+  document.getElementById('app')
+);
